6bb772355c74f574cf5f5735a9c13342574e5d19
ElEsAcosta <39068727+ElEsAcosta@users.noreply.github.com>
2025-07-15 10:46:35 -0500

    Subiendo solución técnica Shopify Gradiweb

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      // 👉 Reglas para JS (con Babel)
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      // 👉 Reglas para SCSS
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  mode: 'development',
+};
